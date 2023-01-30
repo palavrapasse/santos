03cca767ec55e7e5bcf5bd652c3e5571623e2ecc
RuteSantos <1160663@isep.ipp.pt>
2023-01-30 22:24:18 +0000

    response functions - Ok and InternalServerError

---
+++ internal/http/response.go
@@ -0,0 +1,15 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func Ok(ectx echo.Context, i interface{}) error {
+	return ectx.JSON(http.StatusOK, i)
+}
+
+func InternalServerError(ectx echo.Context) error {
+	return ectx.NoContent(http.StatusInternalServerError)
+}
