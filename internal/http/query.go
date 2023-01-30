80caa16d649e76cafd1e4b5104a572cc11b87505
RuteSantos <1160663@isep.ipp.pt>
2023-01-30 22:15:18 +0000

    include query server configs

---
+++ internal/http/query.go
@@ -0,0 +1,17 @@
+package http
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	queryServerHostEnvKey = "query_server_host"
+	queryServerPortEnvKey = "query_server_port"
+)
+
+var (
+	queryServerHost = os.Getenv(queryServerHostEnvKey)
+	queryServerPort = os.Getenv(queryServerPortEnvKey)
+	QueryServiceURL = fmt.Sprintf("%s:%s", queryServerHost, queryServerPort)
+)
