9121239c8edf79fc67ae94dff186fe58c836b741
RuteSantos <1160663@isep.ipp.pt>
2023-01-30 22:59:16 +0000

    include main function

---
+++ cmd/santos/santos.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/labstack/echo/v4"
+	as "github.com/palavrapasse/aspirador/pkg"
+	"github.com/palavrapasse/santos/internal/http"
+	"github.com/palavrapasse/santos/internal/logging"
+)
+
+func main() {
+
+	logging.Aspirador = as.WithClients(logging.CreateAspiradorClients())
+
+	logging.Aspirador.Trace("Starting Santos Service")
+
+	e := echo.New()
+
+	defer e.Close()
+
+	http.RegisterMiddlewares(e)
+	http.RegisterHandlers(e)
+
+	e.Logger.Fatal(http.Start(e))
+}
