400d23092d22da31bb7f7af8d9e105eb3d1a7812
freitzzz <1160907@isep.ipp.pt>
2023-02-11 22:37:33 +0000

    start auto tls server if domain name is provided

--- internal/http/connection.go
+++ internal/http/connection.go
@@ -5,20 +5,34 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 const (
-	serverHostEnvKey = "server_host"
-	serverPortEnvKey = "server_port"
+	serverHostEnvKey                = "server_host"
+	serverPortEnvKey                = "server_port"
+	serverDomainEnvKey              = "server_domain"
+	serverAutoCertDirFilePathEnvKey = "server_autocrt_cache_dir_fp"
 )
 
 var (
-	serverHost = os.Getenv(serverHostEnvKey)
-	serverPort = os.Getenv(serverPortEnvKey)
+	serverHost                      = os.Getenv(serverHostEnvKey)
+	serverPort                      = os.Getenv(serverPortEnvKey)
+	serverDomain                    = os.Getenv(serverDomainEnvKey)
+	serverAutoCertDirectoryFilePath = os.Getenv(serverAutoCertDirFilePathEnvKey)
 )
 
 func Start(e *echo.Echo) error {
-	return e.Start(serverAddress())
+	addr := serverAddress()
+
+	if len(serverDomain) > 0 {
+		e.AutoTLSManager.HostPolicy = autocert.HostWhitelist(serverDomain)
+		e.AutoTLSManager.Cache = autocert.DirCache(serverAutoCertDirectoryFilePath)
+
+		return e.StartAutoTLS(addr)
+	}
+
+	return e.Start(addr)
 }
 
 func serverAddress() string {
